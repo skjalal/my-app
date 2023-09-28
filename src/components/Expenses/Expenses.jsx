5c84f8555e8fc38fa563d96f747d2f9d64e7f78c
Shaikh Jalaluddin <sk.jalal460@gmail.com>
2023-09-29 00:21:55 +0530

    refactor component

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,9 +1,9 @@
 import ExpenseItem from "./ExpenseItem";
-import Card from "./Card";
+import Card from "../UI/Card";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  const expenses = props.expenses;
+  const expenses = props.items;
   return (
     <Card className="expenses">
       <ExpenseItem
