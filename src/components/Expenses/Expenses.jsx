4241d9e0ab3a2feda5f5c619f7c7e2892e487031
Shaikh Jalaluddin <sk.jalal460@gmail.com>
2023-10-02 18:19:28 +0530

    submit new form

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -5,7 +5,7 @@ import "./Expenses.css";
 const Expenses = (props) => {
   const { expenses } = props;
   const items = expenses.map((d) => (
-    <ExpenseItem title={d.title} amount={d.amount} date={d.date} />
+    <ExpenseItem key={d.id} title={d.title} amount={d.amount} date={d.date} />
   ));
   return <Card className="expenses">{items}</Card>;
 };
