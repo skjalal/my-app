3710e9899c5fc36cafa30e085bd94161fb1a8950
Shaikh Jalaluddin <sk.jalal460@gmail.com>
2023-10-03 03:10:30 +0530

    added new panel

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isNewExpense, setIsNewExpense] = useState(true);
   const saveExpenseHandler = (enteredExpenseData) => {
     const expenseData = { ...enteredExpenseData, id: Math.random().toString() };
     props.onAddExpense(expenseData);
+    setIsNewExpense(true);
+  };
+
+  const clickHadnler = () => {
+    setIsNewExpense(false);
+  };
+
+  const cancelHadnler = () => {
+    setIsNewExpense(true);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseHandler} />
+      {isNewExpense && <button onClick={clickHadnler}>Add New Expense</button>}
+      {!isNewExpense && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseHandler}
+          onCancelForm={cancelHadnler}
+        />
+      )}
     </div>
   );
 };
